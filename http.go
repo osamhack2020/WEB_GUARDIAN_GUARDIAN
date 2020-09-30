899acf0beef85080166650ee15e9721d208c88cf
gron1gh1 <gron1gh1@gmail.com>
2020-09-30 23:04:18 +0900

    (Add) gocv module

--- http.go
+++ http.go
@@ -1,39 +1,62 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
 	"log"
-	"time"
+	"net/http"
+
 	gosocketio "github.com/graarh/golang-socketio"
 	"github.com/graarh/golang-socketio/transport"
+	"gocv.io/x/gocv"
 )
 
 func main() {
-		//create
-		server := gosocketio.NewServer(transport.GetDefaultWebsocketTransport())
+	//create
+	server := gosocketio.NewServer(transport.GetDefaultWebsocketTransport())
 
-		//handle connected
-		var global_channel *gosocketio.Channel
-		server.On(gosocketio.OnConnection, func(c *gosocketio.Channel) {
-			log.Println("New client connected")
-			global_channel = c
-		})
+	//handle connected
+	var globalChannel *gosocketio.Channel
+	server.On(gosocketio.OnConnection, func(c *gosocketio.Channel) {
+		log.Println("New client connected")
+		globalChannel = c
+	})
+	//"rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov"
 
-		go func() {
-			i:=0
-			for{
-				if global_channel == nil {
-					continue
-				}
-				global_channel.Emit("data",i)
-				i++
-				time.Sleep(time.Second)
+	cap, err := gocv.OpenVideoCapture("rtsp://13.76.101.16:8089/test")
+
+	if err != nil {
+		fmt.Printf("Error opening capture device")
+		return
+	}
+	defer cap.Close()
+
+	go func() {
+
+		img := gocv.NewMat()
+		defer img.Close()
+
+		for {
+
+			if globalChannel == nil {
+				continue
 			}
-		}()
-		
-		//setup http server
-		serveMux := http.NewServeMux()
-		serveMux.Handle("/socket.io/", server)
-		serveMux.Handle("/", http.FileServer(http.Dir("./asset")))
-		log.Panic(http.ListenAndServe(":80", serveMux))
+			if ok := cap.Read(&img); !ok {
+				fmt.Printf("Device closed\n")
+				return
+			}
+			if img.Empty() {
+				continue
+			}
+
+			buf, _ := gocv.IMEncode(".jpg", img)
+			globalChannel.Emit("frame", buf)
+		}
+	}()
+
+	//setup http server
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/socket.io/", server)
+	serveMux.Handle("/", http.FileServer(http.Dir("./asset")))
+	log.Println("server on 8080!")
+	log.Panic(http.ListenAndServe(":8080", serveMux))
 }
