f093dc5e54e83ffde11d57432eea209260ceaad5
JangSeongHo99 <qq9725@ajou.ac.kr>
2020-10-07 13:35:04 +0000

    Basic server.js using express

---
+++ docker/node/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// app
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
