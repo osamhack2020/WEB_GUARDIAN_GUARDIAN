940f1c2a6456352952958a6d769238c2aded2a95
gron1gh1 <gron1gh1@gmail.com>
2020-10-09 05:58:07 +0000

    Add: LeftBar Logo & LeftBar Collapase

--- src/MainLayout.tsx
+++ src/MainLayout.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from "react";
-import { Layout,  Menu, Breadcrumb } from "antd";
+import { Layout, Menu, Breadcrumb } from "antd";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import {
   DesktopOutlined,
@@ -7,21 +7,29 @@ import {
   SettingOutlined,
 } from "@ant-design/icons";
 import "antd/dist/antd.css";
+import styled from "styled-components"
 import Log from "./Log";
 import "./index.css";
-const { Header, Content, Footer, Sider } = Layout;
 
+const { Content, Footer, Sider } = Layout;
 function LogBar() {
   return (
     <Sider
       style={{ background: "white" }}
-      reverseArrow
       width={300}
     >
       <Log />
     </Sider>
   );
 }
+function Logo() {
+  return (
+
+    <img style={{ width: '64px', height: '64px', marginTop: '5px', marginBottom: '5px' }} src="/logo128.png" />
+  )
+}
+
+
 export default function MainLayout({
   main,
   setting,
@@ -31,7 +39,12 @@ export default function MainLayout({
   setting: React.FunctionComponent;
   dashboard: React.FunctionComponent;
 }) {
- 
+
+  const [LeftBar, SetLeftBar] = useState<boolean>(false);
+
+  const onLeftBar = (collapsed: boolean) => {
+    SetLeftBar(collapsed);
+  };
   const defaultSelecedKey = useCallback(() => {
     const pathname = window.location.pathname;
     if (pathname === "/") return "1";
@@ -44,47 +57,54 @@ export default function MainLayout({
       <Layout
         style={{ height: "100vh", overflowX: "hidden", overflowY: "hidden" }}
       >
-        <Header className="header" style={{background:"#F8EC93"}}>
-          <div className="logo" />
-        </Header>
-     <Layout>
-        <Sider >
-          <Menu
-            theme="dark"
-            defaultSelectedKeys={[defaultSelecedKey()]}
-            mode="inline"
-          >
-            <Menu.Item key="1" icon={<DesktopOutlined />}>
-              CCTV 모니터링
-              <Link to="/" />
-            </Menu.Item>
-            <Menu.Item key="2" icon={<SettingOutlined />}>
-              설정
-              <Link to="/setting" />
-            </Menu.Item>
-            <Menu.Item key="3" icon={<PieChartOutlined />}>
-              통계
-              <Link to="/dashboard" />
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout className="site-layout" >
-          <Content style={{ margin: "0 16px" }}>
-            <div
-              className="site-layout-background"
-              style={{ padding: 24, minHeight: 360 }}
+        <Layout>
+          <Sider width="180" collapsible collapsed={LeftBar} onCollapse={onLeftBar}>
+            <Menu
+              theme="dark"
+              defaultSelectedKeys={[defaultSelecedKey()]}
+              mode="inline"
+              inlineIndent={LeftBar ? 10 : 50}
             >
-              <Route exact path="/" component={main} />
-              <Route path="/setting" component={setting} />
-              <Route path="/dashboard" component={dashboard} />
-            </div>
-          </Content>
-          <Footer style={{ textAlign: "center", fontWeight: "bold" }}>
-            GUARDIAN - CCTV 감지체계
+              <Menu.Item key="logo" style={LeftBar ? { paddingLeft: '10px', height: '70px', cursor: 'default', opacity: 1.0 } : { height: '70px', cursor: 'default', opacity: 1.0 }} icon={<Logo />} disabled>
+              </Menu.Item>
+            </Menu>
+            <Menu
+              theme="dark"
+              defaultSelectedKeys={[defaultSelecedKey()]}
+              mode="inline"
+            >
+
+              <Menu.Item key="1" icon={<DesktopOutlined />}>
+                CCTV 모니터링
+              <Link to="/" />
+              </Menu.Item>
+              <Menu.Item key="2" icon={<SettingOutlined />}>
+                설정
+              <Link to="/setting" />
+              </Menu.Item>
+              <Menu.Item key="3" icon={<PieChartOutlined />}>
+                통계
+              <Link to="/dashboard" />
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout className="site-layout" >
+            <Content style={{ margin: "0 16px" }}>
+              <div
+                className="site-layout-background"
+                style={{ padding: 24, minHeight: 360 }}
+              >
+                <Route exact path="/" component={main} />
+                <Route path="/setting" component={setting} />
+                <Route path="/dashboard" component={dashboard} />
+              </div>
+            </Content>
+            <Footer style={{ textAlign: "center", fontWeight: "bold" }}>
+              GUARDIAN - CCTV 감지체계
           </Footer>
+          </Layout>
+          <Route exact path="/" component={LogBar} />
         </Layout>
-        <Route exact path="/" component={LogBar} />
-      </Layout>
       </Layout>
     </Router>
   );
--- src/Setting.tsx
+++ src/Setting.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React,{useEffect,useRef} from "react";
 
 export default function Setting()
 {
+  const ref = useRef();
+  // useEffect(()=>{
+  //   let ctx : any = document.getElementById('canvas').getContext('2d');
+  //   let img  :any = new Image();
+  //     img.onload = function() {
+  //       ctx.drawImage(img, 0, 0);
+  //       ctx.beginPath();
+  //       ctx.moveTo(30, 96);
+  //       ctx.lineTo(70, 66);
+  //       ctx.lineTo(103, 76);
+  //       ctx.lineTo(170, 15);
+  //       ctx.stroke();
+  //     };
+  //     img.src = "https://via.placeholder.com/480x270"
+  // },[])
   return (
-    <p>Setting</p>
+    <canvas
+    id='canvas'
+    style={{width:480,height:270,cursor:'pointer'}}
+   
+  />
   )
 }
