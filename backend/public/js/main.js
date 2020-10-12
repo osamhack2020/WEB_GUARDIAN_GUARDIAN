773ac88e22a4034fa488ce516da70198d7ac17f1
me9min <zlws554@gmail.com>
2020-10-13 00:12:12 +0900

    add: 상황일지 소켓통신구현
    (테스트용)클라이언트 -> 서버 -> 클라이언트
    서버 -> 클라이언트
    로 json 메세지날리는기능 추가

--- backend/public/js/main.js
+++ backend/public/js/main.js
@@ -1,29 +1,45 @@
+var socket = io();
+
 $(document).ready(function() {
-	var socket = io();
-
-	// 메세지보내기 기능 추가 (room= 이벤트네임, msg= 보낼내용)
-	function sendMsg(room, msg) {
-		socket.emit(room, msg);
-	};
-
-	// 메세지 디스플레이 기능
-	function displayMsg(clas, msg) {
-		$(clas).append($('<span>'+msg+'</span>'));
-	};
 
 	// CCTV일지 메세지가 올시 로그메뉴에 메세지추가
 	socket.on("log", function(data) {
-		displayMsg(".log",data);
+		var msg = JSON.parse(data);
+
+		msg =
+			'<span>'+
+				'<img alt="404 error" src="'+msg.msg_thumbnail+'">'+
+				msg.msg_text+
+			'</span>'
+		;
+		console.log(msg);
+
+		// 받은메세지 디스플레이
+		$(".log").append(msg);
 	});
 
 	// 테스트 메세지 전송기능
 	function log_input() {
-		sendMsg("log_input",$(".msg").val());
-		$(".msg").val("");
+		var msg_text = $(".msg_text").val();
+
+		if (msg_text.length > 0) {
+
+			var msg = '{"msg_thumbnail":"'+$('.msg_thumbnail').val()+'","msg_text":"'+msg_text+'"}';
+			socket.emit("log_input",msg);
+
+			$(".msg_thumbnail").val("");
+			$(".msg_text").val("");
+		} else {
+
+			alert('내용이없습니다');
+			$(".msg_text").focus();
+		}
 	}
 
 	// 테스트 메세지 전송, 버튼클릭 or 엔터 누르면
 	$(".send_msg").click(function() { log_input(); });
-	$(".msg").keyup(function(e) { if(e.keyCode == 13) {log_input();} });
+	$(".msg_text").keyup(function(e) { if(e.keyCode == 13) {log_input();} });
 
+	// 테스트
+	socket.emit("log_input",'{"msg_thumbnail":"/img/osam.png","msg_text":"test text"}');
 });
