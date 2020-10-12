126fd7308fce5a6c2f8f2fc1d3026bbc348c5bfb
gron1gh1 <gron1gh1@gmail.com>
2020-10-12 11:15:28 +0000

    Fix: end ing time+

--- cv_util.go
+++ cv_util.go
@@ -177,6 +177,7 @@ func DetectArea(img gocv.Mat, info DetectPointInfo) gocv.Mat {
 	result := gocv.NewMatWithSize(imgClone.Rows(), imgClone.Cols(), gocv.MatTypeCV8UC1)
 	gocv.FillPoly(&mask, info.DetectPoint, color.RGBA{255, 255, 255, 0})
 	imgClone.CopyToWithMask(&result, mask)
+	return result
 	boundingRect := gocv.BoundingRect(gocv.FindContours(mask.Clone(), gocv.RetrievalExternal, gocv.ChainApproxSimple)[0])
 	return result.Region(boundingRect)
 }
@@ -232,13 +233,16 @@ func DetectStart(CapUrl string, Server *gosocketio.Server, DetectPointChannel ch
 		var startFlag bool
 		for img := range FrameChannel {
 			//gocv.Resize(img, &img, image.Point{1280, 720}, 0, 0, 1)
-			motion, motionCnt := MotionDetect(img, mog2)
+			_, motionCnt := MotionDetect(img, mog2)
 			if motionCnt > 0 { // 움직임 감지됐으면
 				if !startFlag { // 움직임 감지 시작 시간 대입
 					startFlag = true
 					fmt.Println("감지 시작")
 					startTime = time.Now()
 
+					// go func() {
+					// 	YoloChannel <- img
+					// }()
 				} else {
 					ingTime := time.Since(startTime)
 					var i time.Duration = 1
@@ -254,17 +258,19 @@ func DetectStart(CapUrl string, Server *gosocketio.Server, DetectPointChannel ch
 			} else { // 움직임 감지없으면
 
 				endTime = time.Since(startTime)
+
 				if startFlag {
+
+					if endTime > time.Second*3 { // 움직임 분기 (움직임 감지 3초 유지)
+						// Process
+						fmt.Printf("감지끝 %d초 이상 유지됨.\n", 3)
+					}
 					startFlag = false
-				}
-				if endTime > time.Second*3 { // 움직임 분기 (움직임 감지 3초 유지)
-					// Process
-					fmt.Printf("감지끝 %d초 이상 유지됨.\n", 3)
 				}
 
 			}
-			buf, _ := gocv.IMEncode(".jpg", motion)
-			ViewChannel <- buf
+			// buf, _ := gocv.IMEncode(".jpg", motion)
+			// ViewChannel <- buf
 		}
 	}()
 	var DPI DetectPointInfo
